c7e026970bb94162a7a47b706032733a767a0210
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-10 14:35:05 +0000

    datatype

--- .devcontainer/part 1/part1.js
+++ .devcontainer/part 1/part1.js
@@ -1,1 +1,9 @@
-console.log("hii");
+const accountid = 10001;
+let accountemail ="username@gmail.com";
+var acountpassword = "usera@123";
+
+acountcity = "bhopal";
+
+console.log(accountid);
+console.table([accountid,accountemail,acountpassword]);
+
