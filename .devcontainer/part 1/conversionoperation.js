ef02d2e3542b057f11359321e4f2d254f864493e
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-11 09:08:47 +0000

    conversion and datatypes

---
+++ .devcontainer/part 1/conversionoperation.js
@@ -0,0 +1,25 @@
+let num = 10;
+let num1 = -num;
+// console.log(num1);
+
+let str1 = "hello";
+let str2 = " sugandh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log(1+2+"3");
+// console.log("1"+"2"+3);
+
+// console.log((1+2)*4/2);
+
+// console.log(+true);
+// console.log(+"");
+
+let counter = 100;
+// counter++;
+// console.log(counter);
+
+// counter--;
+// console.log(counter);
