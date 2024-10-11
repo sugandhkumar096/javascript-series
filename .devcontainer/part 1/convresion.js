c54cb67926d4e8b83e03eb4b8cf6bd0a4bb50a85
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-11 08:11:57 +0000

    conversion

---
+++ .devcontainer/part 1/convresion.js
@@ -0,0 +1,22 @@
+let score = "sugandh";
+// console.log((typeof score));
+// console.log(typeof(score));
+
+let valuenumber = Number(score);
+// console.log((typeof valuenumber));
+// console.log(valuenumber);
+
+//"33abc" =NaN
+
+let isloggedIn = "sugandh";
+let booleanIslogedIn = Boolean(isloggedIn);
+// console.log(booleanIslogedIn);
+
+let somenumber = 33;
+
+let stringnumber = String(somenumber);
+console.log((typeof stringnumber));
+console.log(stringnumber);
+
+
+
