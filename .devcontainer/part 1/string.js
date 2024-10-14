8896c5195cb0af7e4c2ca6de61edade743f69693
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-14 16:23:28 +0000

    string configuration

---
+++ .devcontainer/part 1/string.js
@@ -0,0 +1,39 @@
+const name = "sugandh";
+const repocount = 20;
+
+// console.log(name + repocount +"value");
+// console.log(`my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String('sugandh');
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('g'));
+
+
+//to recall a string function and properties
+
+const newString = gamename.substring(0,4);
+// console.log(newString);
+
+const anotherString = gamename.slice(-8,4);
+// console.log(anotherString);
+
+const newStringone = " sugandh  ";
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+const url = "https://sugandh.com/sugandh%20mehta";
+// console.log(url.replace('%20','__'));
+// console.log(url.includes("handsome"));
+// console.log(url.includes('sugandh'));
+
+// console.log(gamename.split("_"));
+
+const nam = "sugandh_mehta";
+// console.log(nam.split(nam[2]));
+
