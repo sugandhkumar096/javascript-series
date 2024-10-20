4efc9496d946699432d47080050a6d41e227a593
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-20 14:18:23 +0000

    function in js

---
+++ .devcontainer/part 1/function.js
@@ -0,0 +1,86 @@
+ 
+//  function sum (num1,num2) {
+//     return num1 + num2;
+    
+//  }
+//   const num =sum(2,4);
+//   console.log(num);
+
+
+// function birthday() {
+//     console.log("happy birthday to you.....");
+    
+// }
+// birthday();
+// birthday();
+
+// to check even or odd number 
+
+// function iseven(num) {
+//     if(num%2 === 0){
+//         return true;
+//     }
+//     else 
+//     return false;
+// }
+// evenNum = iseven(4);
+
+// console.log(evenNum);
+  
+// function valueChk(array , target) {
+//     for(let i = 0; i<5; i++){
+//     if (array[i] === target) {
+//         return i;
+        
+//     }
+// }
+//     return -1;
+    
+// }
+// const array = [2,4,5,6,7,8];
+// const value = valueChk(array,5);
+// console.log(value);
+
+//function expression 
+
+// const sum = function (a,b) {
+//     return a+ b;
+
+    
+// }
+// const add = sum(4,6);
+// console.log(add);
+
+//arrow function 
+// const  iseven = (num) => {
+//         if(num%2 === 0){
+//             return true;
+//         }
+//         else 
+//         return false;
+//     }
+//     evenNum = iseven(4);
+    
+//     console.log(evenNum);
+
+//     const  sum =  (num1,num2) => {
+//             return num1 + num2;
+            
+//          }
+//           const num =sum(2,4);
+//           console.log(num);
+
+//         const even = num => num%2 === 0;
+         
+//         console.log(iseven(40));
+
+//     const firstString = string => string[0];
+
+//     console.log(firstString("sugandh"));
+            
+
+          
+
+
+
+ 
