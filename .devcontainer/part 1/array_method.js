e46b147d5db384e9972c72d7385543f1fbf4e107
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-10-21 17:43:16 +0000

    array method

---
+++ .devcontainer/part 1/array_method.js
@@ -0,0 +1,15 @@
+// const array = [4,5,6,23,4,45];
+
+// function fun1(array,index) {
+//     console.log(`the number is ${array} and the index is ${index}`);
+// }
+
+// // for (let i = 0; i < array.length; i++) {
+// //     fun1(array[i],i);
+    
+// // }
+
+// // array.forEach(fun1);
+// array.forEach(function(array,index){
+//     console.log(`the number is ${array} and the index is ${index}`);
+//     });
