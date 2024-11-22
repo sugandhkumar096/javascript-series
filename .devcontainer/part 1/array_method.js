9731d777a3f8492134f91415f57b87978624d3fe
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-22 16:49:20 +0000

    filter method

--- .devcontainer/part 1/array_method.js
+++ .devcontainer/part 1/array_method.js
@@ -15,14 +15,22 @@
 //     });
 
 
+// filter method 
 
+const numbers = [1,3,2,6,4,8];
+
+
+
+const evenNumbers = numbers.filter((number)=>{
+    return number % 2 === 0;
+});
+console.log(evenNumbers);
 
 //reduce method
+// const number = [2,3,4,5,3,9];
+// const sum = number.reduce((accumulator,currentvalue)=>{
+//     return accumulator+currentvalue;
 
-const number = [2,3,4,5,3,9];
-const sum = number.reduce((accumulator,currentvalue)=>{
-    return accumulator+currentvalue;
 
-
-});
-console.log(sum);
+// });
+// console.log(sum);
