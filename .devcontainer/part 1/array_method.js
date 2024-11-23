8ebdbcc9c7640e80f603468012b17b1d6cf49d17
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-23 06:32:55 +0000

    sort method in array

--- .devcontainer/part 1/array_method.js
+++ .devcontainer/part 1/array_method.js
@@ -17,14 +17,14 @@
 
 // filter method 
 
-const numbers = [1,3,2,6,4,8];
+// const numbers = [1,3,2,6,4,8];
 
 
 
-const evenNumbers = numbers.filter((number)=>{
-    return number % 2 === 0;
-});
-console.log(evenNumbers);
+// const evenNumbers = numbers.filter((number)=>{
+//     return number % 2 === 0;
+// });
+// console.log(evenNumbers);
 
 //reduce method
 // const number = [2,3,4,5,3,9];
@@ -34,3 +34,44 @@ console.log(evenNumbers);
 
 // });
 // console.log(sum);
+
+// const usercart = [
+//     {productId: 1, productName: "samsung",price: 12000},
+//     {productId: 2, productName:"vivo", price: 14000},
+//     {productId:3, productName: "realme", price:10000}
+// ]
+// const totalAmount = usercart.reduce((totalPrice,currentvalue)=>{
+//     return totalPrice + currentvalue.price;
+// },0);
+
+// console.log(totalAmount);
+
+// sort method
+ //const number = [3,177,87,89,2,12,34];
+// number.sort();
+//  console.log(number);
+
+// const sorting = number.sort((a,b)=>{
+//     // return a-b;
+//     //return b-a;
+// });
+// console.log(number);
+
+// const product = [
+//     {productId : 1,productname :"laptop", price: 1000},
+//     {productId : 2,productname :"mobile", price: 12000},
+//     {productId : 3,productname :"earphone", price: 500},
+//     {productId : 4,productname :"headphone", price: 1000},
+//     {productId : 5,productname :"invertor", price: 10000},
+// ]
+
+//   product.sort((a,b) =>{
+//     // return a.price - b.price;
+//     return b.price - a.price;
+//   });
+
+//   console.log(product);
+  
+
+
+ 
