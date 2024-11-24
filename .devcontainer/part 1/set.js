3e39738c04b414d21944c9b80360b0ac1ee3d7c2
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-24 05:53:49 +0000

    sets in javascripts

---
+++ .devcontainer/part 1/set.js
@@ -0,0 +1,32 @@
+// sets
+
+//const myArray = new Set([1,2,3]);
+//const myArray = new Set("sugandh");
+
+// const myArray = new Set();
+//  myArray.add(3);
+//  myArray.add(5);
+//  myArray.add(6);
+//  myArray.add(4);
+//  myArray.add(1);
+// //  if (myArray.has(1)) {
+// //     console.log("1 is present ");      
+// //  }
+// //  else{
+// //     console.log("1 is not present");
+// //  }
+// for(let number of myArray){
+//     console.log(myArray);
+    
+// }
+
+// //console.log(myArray);
+// const number = [1,2,3,4,3,2,5,6,7,8];
+//  const uniqueElement = new Set(number);
+// // console.log(uniqueElement);
+
+// let length = 0;
+// for(let number of uniqueElement ){
+//     length++;
+// }
+// console.log(length);
