cfad4a463cc8b6510857812b31b63af0740652b6
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-24 05:17:58 +0000

    parts of iterable and array like object

---
+++ .devcontainer/part 1/iterable.js
@@ -0,0 +1,19 @@
+// iterable
+
+// const firstName = ["sugandh"];
+// for(let char of firstName){
+//     console.log(firstName);
+    
+// }
+
+// const myArray = ["item1","item2","item3","item4"];
+// for(let item of myArray ){
+//     console.log(item);
+// }
+
+// array like object
+
+// const myArray = "sugandh";
+// console.log(myArray.length);
+// console.log(myArray[2]);
+
