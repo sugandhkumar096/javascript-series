2f0f565278aaca58c70bacc1e7d53a8a39c1f875
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-24 16:36:13 +0000

    map with get

--- .devcontainer/part 1/object_map.js
+++ .devcontainer/part 1/object_map.js
@@ -1,6 +1,28 @@
-const person = new Map();
-person.set("firstname","sugandh");
-person.set("age",21);
-person.set(1,'one');
+// const person = new Map();
+// person.set("firstname","sugandh");
+// person.set("age",21);
+// person.set(1,'one');
 
-console.log(person);
+// console.log(person);
+
+// const person = new Map([["firstname","sugandh"],["age",21],["gender","male"]]);
+// console.log(person);
+
+// const person1 = {
+//     id: 1,
+//     firstname: "sugandh",
+
+// }
+// const person2 ={
+//     id: 2,
+//     firstname:"deepak",
+// }
+// const extrainfo = new Map();
+// extrainfo.set(person1,{age: 12, gender:"male"});
+// extrainfo.set(person2,{age: 13, gender:"male"});
+// //console.log(extrainfo);
+// console.log(person1.id);
+// console.log(extrainfo.get(person1).gender);
+// console.log(extrainfo.get(person2).gender);
+
+
