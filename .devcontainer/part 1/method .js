daa957e998975762e27cb4125f7ed9c1edfe06c6
sugandhkumar096 <sugandhkumarkvs096@gmail.com>
2024-11-25 17:31:43 +0000

    this keyword

---
+++ .devcontainer/part 1/method .js
@@ -0,0 +1,36 @@
+// // methods
+
+// // const person = {
+// //     name : "sugandh",
+// //     age: 21,
+// //     about: function(params) {
+// //         console.log(`my name is ${this.name} and my age is ${this.age}`);
+        
+// //     }
+// // }
+// // person.about();
+
+// function personinfo() {
+//     console.log(`my name is ${this.name}, and my age is ${this.age}`);
+    
+    
+// }
+// const person1 = {
+//     name: "sugandh",
+//     age: 21,
+//     about: personinfo
+// }
+// const person2 = {
+//     name: "deepak",
+//     age: 16,
+//     about: personinfo
+// }
+// const person3 = {
+//     name: "subham",
+//     age: 8,
+//     about: personinfo
+// }
+
+// person1.about();
+// person2.about();
+// person3.about();
--- .devcontainer/part 1/nums_and_maths.js
+++ .devcontainer/part 1/nums_and_maths.js
@@ -1,14 +1,14 @@
-const score = 400;
-// console.log(score);
+// const score = 400;
+// // console.log(score);
 
-const balance = new Number(100);
-// console.log(balance);
+// const balance = new Number(100);
+// // console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(2));
 
-const othernum = 132.569;
-// console.log(othernum.toPrecision(4));
+// const othernum = 132.569;
+// // console.log(othernum.toPrecision(4));
 
-const hundred = 100000;
-console.log(hundred.toLocaleString('en-In'));
+// const hundred = 100000;
+// console.log(hundred.toLocaleString('en-In'));
